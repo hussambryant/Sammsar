804d892ec4ba040c7366b6f48374d50cbf965df5
hussambryant <24hussam24@gmail.com>
2014-02-14 13:00:32 +0200

    Initial commit

---
+++ Sammsar.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sammsar.Web.MvcApplication" Language="C#" %>
---
+++ Sammsar.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sammsar.Web.Startup))]
+namespace Sammsar.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
